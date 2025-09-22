68a73065ec31d99c04bc13e11e153806e991839e
qalqa-dev <113594321+qalqa-dev@users.noreply.github.com>
2025-09-22 21:21:53 +0300

    task completed

---
+++ create-phone-number.js
@@ -0,0 +1,8 @@
+const createPhoneNumber = (numbers) =>
+  `(${numbers.slice(0, 3).join('')}) ${numbers.slice(3, 6).join('')}-${numbers
+    .slice(6)
+    .join('')}`;
+
+console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));
+
+const plural = (n) => n !== 1;
