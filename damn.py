bd03c591ddf4135ab158ccb2cddf9c9935d40e95
qalqa-dev <113594321+qalqa-dev@users.noreply.github.com>
2025-09-26 14:32:21 +0300

    fuck that shit

---
+++ damn.py
@@ -0,0 +1,14 @@
+def cursed_world():
+    return (chr(((((chr==chr)+(chr==chr)+(chr==chr))**((chr==chr)+(chr==chr)))+(chr==chr))**((chr==chr)+(chr==chr))+(chr==chr)+(chr==chr)+(chr==chr)+(chr==chr)) + 
+            chr(((((chr==chr)+(chr==chr)+(chr==chr))**((chr==chr)+(chr==chr)))+(chr==chr))**((chr==chr)+(chr==chr))+(chr==chr)) + 
+            chr(((((chr==chr)+(chr==chr)+(chr==chr))**((chr==chr)+(chr==chr)))+(chr==chr))**((chr==chr)+(chr==chr))+((chr==chr)+(chr==chr))**((chr==chr)+(chr==chr)+(chr==chr))) +  
+            chr(((((chr==chr)+(chr==chr)+(chr==chr))**((chr==chr)+(chr==chr)))+(chr==chr))**((chr==chr)+(chr==chr))+((chr==chr)+(chr==chr))**((chr==chr)+(chr==chr)+(chr==chr))) + 
+            chr(((((chr==chr)+(chr==chr)+(chr==chr))**((chr==chr)+(chr==chr)))+(chr==chr))**((chr==chr)+(chr==chr))+(((chr==chr)+(chr==chr)+(chr==chr))**((chr==chr)+(chr==chr)))+(chr==chr)+(chr==chr)) + 
+            chr(((chr==chr)+(chr==chr))**((chr==chr)+(chr==chr)+(chr==chr))*((chr==chr)+(chr==chr)+(chr==chr)+(chr==chr))) +  
+            chr(((((chr==chr)+(chr==chr)+(chr==chr))**((chr==chr)+(chr==chr)))+(chr==chr))**((chr==chr)+(chr==chr))+((chr==chr)+(chr==chr)+(chr==chr)+(chr==chr))**((chr==chr)+(chr==chr))+(chr==chr)+(chr==chr)+(chr==chr)) + 
+            chr(((((chr==chr)+(chr==chr)+(chr==chr))**((chr==chr)+(chr==chr)))+(chr==chr))**((chr==chr)+(chr==chr))+(((chr==chr)+(chr==chr)+(chr==chr))**((chr==chr)+(chr==chr)))+(chr==chr)+(chr==chr)) + 
+            chr(((((chr==chr)+(chr==chr)+(chr==chr))**((chr==chr)+(chr==chr)))+(chr==chr))**((chr==chr)+(chr==chr))+((chr==chr)+(chr==chr)+(chr==chr))**((chr==chr)+(chr==chr))+(chr==chr)+(chr==chr)+(chr==chr)+(chr==chr)+(chr==chr)) + 
+            chr(((((chr==chr)+(chr==chr)+(chr==chr))**((chr==chr)+(chr==chr)))+(chr==chr))**((chr==chr)+(chr==chr))+((chr==chr)+(chr==chr))**((chr==chr)+(chr==chr)+(chr==chr))) + 
+            chr(((((chr==chr)+(chr==chr)+(chr==chr))**((chr==chr)+(chr==chr)))+(chr==chr))**((chr==chr)+(chr==chr))))
+
+print(cursed_world())
