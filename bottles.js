3514025a5f9ce5f9f80dfc701a1e2783d99ebfb2
qalqa-dev <113594321+qalqa-dev@users.noreply.github.com>
2025-10-01 12:40:14 +0300

    task completed

---
+++ bottles.js
@@ -0,0 +1,12 @@
+var numWaterBottles = function (numBottles, numExchange) {
+  let count = numBottles;
+  let emptyBottles = numBottles;
+  while (emptyBottles >= numExchange) {
+    const newBottles = Math.floor(emptyBottles / numExchange);
+    count += newBottles;
+    emptyBottles = newBottles + (emptyBottles % numExchange);
+  }
+  return count;
+};
+
+console.log(numWaterBottles(9, 3));
